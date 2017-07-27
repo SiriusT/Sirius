ae7f9cbc615365e955a9faea9ab5db717d6bc8c4
SiriusT <30367076+SiriusT@users.noreply.github.com>
2017-07-27 14:07:27 +0800

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.star.myapplication;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private IntentFilter intentFilter;
+    private NetWorkChangeReceiver netWorkChangeReceiver;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        intentFilter = new IntentFilter();
+        intentFilter.addAction("android.net.conn.CONNECTIVITY_CHANGE");
+        netWorkChangeReceiver = new NetWorkChangeReceiver();
+        registerReceiver(netWorkChangeReceiver, intentFilter);
+    }
+
+    protected  void onDestroy(){
+        super.onDestroy();
+        unregisterReceiver(netWorkChangeReceiver);
+    }
+
+    class NetWorkChangeReceiver extends BroadcastReceiver{
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            ConnectivityManager connectionManager = (ConnectivityManager)
+                    getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo networkInfo = connectionManager.getActiveNetworkInfo();
+            if (networkInfo != null && networkInfo.isAvailable()){
+                Toast.makeText(context, "network is available",
+                        Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(context, "network is unavailable",
+                        Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+}
